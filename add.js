33b08e2f99e60ecdedb8fdfc9ea923fb80a27348
JaxonsherZievudinov <zievudinovdzaha@gmail.com>
2024-12-20 18:06:01 +0500

    first commit

---
+++ add.js
@@ -0,0 +1,54 @@
+// task1
+// function sum(a) {
+//     let b=0
+//          for(let i=0; i<a.length; i++){
+//           b+=a[i]    
+//          }
+//          return b
+// }
+// console.log(sum([2,4,6,8,10]));
+
+// task2
+// function sum(a) {
+//     let b=-99999
+//     for(let i=0; i<a.length; i++){
+//         if(a[i]>b){
+//             b=a[i]
+//         }
+//     }
+//     return b
+// }
+// console.log(sum([1,2,3,4,5,6,7,8]));
+
+// task3
+// function sum(a) {
+//     let b=0
+//     for(let i = 0; i < a.length; i++){
+//         if(a[i]%2==0){
+//             b++
+//         }
+//     }
+//     return b
+// }
+// console.log(sum([1,2,3,4,5]));
+
+// task4
+// function sum(a,b) {
+//     return a.includes(b)
+// }
+// console.log(sum([1,2,3,4,5],8));
+
+
+// task5
+// function sum(a) {
+//     let b=0
+//     let sum=0
+//     for(let i=0; i<a.length; i++){
+//         sum+=a[i]
+//         b++
+//     }
+//     return sum/b
+// }
+// console.log(sum([1,4,6,9,10]));
+
+
